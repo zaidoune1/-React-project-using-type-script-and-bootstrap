c5da30b6d312a8dd440e5197c424a84047a70340
Zaidoun Moez <81720068+zaidoune1@users.noreply.github.com>
2023-02-19 21:29:53 +0100

    Update CardAllItems.tsx

--- src/compounents/CardAllItems.tsx
+++ src/compounents/CardAllItems.tsx
@@ -22,16 +22,24 @@ console.log(data)
         if(item == null) return null
 
     return (
-        <Stack style={{display:"flex", justifyContent:"space-between", alignItems:"center"}} direction="horizontal" gap={2} >
-                <img style={{width:"100px", height:"80px", objectFit:"cover"}} src={item.imgUrl} />
-            <div className="d-flex flex-column">
-                <div> {item.name}</div>
-                <div> {FormateCurrency(item.price)}</div>
-                <div> {quantity}</div>
-            </div>
+         <Stack style={{display:"flex", justifyContent:"space-between", alignItems:"center"}} direction="horizontal" gap={2} >
+            <div className="d-flex justify-content center align-items-center">
+            <div><img style={{width:"100px", height:"80px", objectFit:"cover"}} src={item.imgUrl} /></div>
+            <div style={{paddingLeft:"10px"}} className="d-flex flex-column">
+            <div> {item.name}</div>
+                <div> {FormateCurrency(item.price * quantity)}</div>
+                {quantity > 1 ? 
+                (<span className="text-muted"> x{quantity} </span>) :(
+                   <span>{quantity}</span>
+                )
+            }
+            </div>  
+           </div>
+        
             <Button onClick={() => {
                 removeItem(item.id)
             }} style={{fontSize:"25px", fontWeight:"bold"}} className="d-flex justify-content center align-items-center" variant="outline-primary"><IoIosRemoveCircle/></Button>
+        
         </Stack>
     )
     }
